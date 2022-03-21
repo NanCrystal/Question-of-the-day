2529b27118e0217da7452a5e7339e6abb3a34237
nannanli <li.nannan@intellif.com>
2022-03-21 17:28:10 +0800

    add first

---
+++ index.js
@@ -0,0 +1,16 @@
+/**
+ * 1. 两数之和
+ * 输入：nums = [2,7,11,15], target = 9
+ * 输出：[0,1]
+ * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 
+ * 
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+ var twoSum = function (nums, target) {
+    // nums.reduce((acc, cur, index) => {
+
+    // }, 0)
+};
+twoSum([2, 7, 11, 15])
